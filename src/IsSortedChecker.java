c228f81ce8b31b0176f7d889dac5b36f523b85f6
Nora P <veganmuffcakes@gmail.com>
2020-06-12 10:49:52 -0700

    Add exisiting Java project files for assignment 8

---
+++ src/IsSortedChecker.java
@@ -0,0 +1,30 @@
+/**
+ * IsSortedChecker.java
+ */
+
+/***************************************************************************
+ * METHOD START LINE 78 BJP5 Exercise 16.3: Method isSorted 
+ * 
+ * REQUIREMENTS
+ * - This method has been added to the LinkedIntList class defined in Ch6
+ * http://www.buildingjavaprograms.com/code-files/5ed/ch16/LinkedIntList2.java
+ **************************************************************************/
+/**
+ * This program tests the isSorted method on a list to determine if the list is
+ * sorted (nondecreasing order). This implementation considers an empty list as
+ * sorted.
+ * 
+ * @author Nora P.
+ * @version 06/12/20
+ */
+public class IsSortedChecker {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
