eb3ab6beaec94b990380d2ed1f0eaaf627a1fd00
Nora P <veganmuffcakes@gmail.com>
2020-06-13 08:53:40 -0700

    working version of isSorted method in LinkedIntList.java, tested in IsSortedChecker.java

--- src/IsSortedChecker.java
+++ src/IsSortedChecker.java
@@ -7,7 +7,7 @@
  * 
  * REQUIREMENTS
  * - This method has been added to the LinkedIntList class defined in Ch6
- * http://www.buildingjavaprograms.com/code-files/5ed/ch16/LinkedIntList2.java
+ * http://www.buildingjavaprograms.com/code-files/5ed/ch16/LinkedInt.java
  **************************************************************************/
 /**
  * This program tests the isSorted method on a list to determine if the list is
@@ -23,8 +23,24 @@ public class IsSortedChecker {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+        LinkedIntList list1 = new LinkedIntList();
+        list1.add(18);
+        list1.add(27);
+        list1.add(93);
+        list1.add(212);
+        
+        System.out.println("Method isSorted Test:");
+        System.out.println("Original list sequence" + list1);
+        System.out.println("isSorted >>> " + list1.isSorted());
+        System.out.println();
+        
+        list1.add(12);
+        list1.add(2);
+        list1.set(1, 3);
+        
+        System.out.println("The List has been modified.");
+        System.out.println("isSorted >>> " + list1.isSorted());
 	}
 
 }
